47e665f068c2a8b4a5d127481a7da60d3c03dc10
meta-krishan-kataria <krishan.kataria@metacube.com>
2016-07-20 10:58:46 +0530

    Update TestQueensProblem.java

--- Assignment-4/TestQueensProblem.java
+++ Assignment-4/TestQueensProblem.java
@@ -10,24 +10,36 @@ public class TestQueensProblem {
             {0, 0, 0, 0},
             {0, 0, 0, 0}
         };
-	int table2[][] = {{0, 0, 0, 0},
-            {0, 0, 0, 0},
-            {0, 0, 0, 0},
-            {0, 0, 0, 0}
+	int table2[][] = {{0, 0, 0, 0,0},
+            {0, 0, 0, 0,0},
+            {0, 0, 0, 0,0},
+            {0, 0, 0, 0,0},
+            {0, 0, 0, 0,0}
         };
 	int[][] res1={{ 0,0,1,0},{ 1,  0,  0,  0},{ 0,  0,  0,  1},{ 0,  1,  0,  0}};
 	
-	QueensProblem ob=new QueensProblem(4);
+	QueensProblem ob1=new QueensProblem(4);
 	QueensProblem ob2=new QueensProblem(5);
+	QueensProblem ob3=new QueensProblem(5);
+	
 	
 	@Test
 	public void test() {
-		assertEquals(true,ob.solveProblem(table, 0));
-		ob.printSolution(table);
+		assertEquals(true,ob1.solveProblem(table, 0));
+		ob1.printSolution(table);
 		for(int i=0; i<4 ;i++){
 			assertArrayEquals(res1[i], table[i]);
 		}
 		
-		assertEquals(false,ob2.solveProblem(table2, 0));
+		
+		//assertEquals(true,ob2.solveProblem(table2, 0));
+		
+		System.out.println();
+		assertEquals(true,ob2.solveProblem(table2, 0));
+		ob2.printSolution(table2);
+		//for(int i=0; i<5 ;i++){
+			//assertArrayEquals(res1[i], table[i]);
+		//}
+		
 	}
 	}
