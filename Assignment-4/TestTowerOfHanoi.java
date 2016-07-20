de0fa36435fadee2a7f0931918d01d1539333897
meta-krishan-kataria <krishan.kataria@metacube.com>
2016-07-20 07:30:09 +0530

    Update TestTowerOfHanoi.java

--- Assignment-4/TestTowerOfHanoi.java
+++ Assignment-4/TestTowerOfHanoi.java
@@ -7,20 +7,26 @@ public class TestTowerOfHanoi {
 
 	@Test
 	public void testSolveTOH() {
-		TowerOfHanoi toh1=new TowerOfHanoi();
-		TowerOfHanoi toh2=new TowerOfHanoi();
+		TowerOfHanoi toh1=new TowerOfHanoi(3);
+		TowerOfHanoi toh2=new TowerOfHanoi(2);
+		TowerOfHanoi toh3=new TowerOfHanoi(1);
+		
+		
 		
 		String[] expected1= {"A->C","A->B","C->B","A->C","B->A","B->C","A->C"};
 		String[] result1=new String[7];
-		assertArrayEquals(expected1, toh1.solve(3, "A", "B", "C", result1));
+		toh1.tower(3, 'A', 'C', 'B');
+		assertArrayEquals(expected1,toh1.result );
 		
 		String[] expected2= {"A->B","A->C","B->C"};
 		String[] result2= new String[3];
-		assertArrayEquals(expected2, toh2.solve(2, "A", "B", "C", result2));
+		toh2.tower(2, 'A', 'C', 'B');
+		assertArrayEquals(expected2,toh2.result );
 		
 		String[] expected3= {"A->C"};
-		String result3[]= new String[1];;
-		assertArrayEquals(expected3, new TowerOfHanoi().solve(1, "A", "B", "C", result3));
+		String result3[]= new String[1];
+		toh3.tower(1, 'A', 'C', 'B');
+		assertArrayEquals(expected3, toh3.result);
 	}
 
 }
