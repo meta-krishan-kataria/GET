e4ee93771cee5495a968d38280a4d6d751978a3f
meta-krishan-kataria <krishan.kataria@metacube.com>
2016-07-28 13:21:26 +0530

    Update DriverClass.java

--- OOP/Social-Network/DriverClass.java
+++ OOP/Social-Network/DriverClass.java
@@ -1,12 +1,9 @@
 
-public class DriverClass {
+ class DriverClass {
 
 	public static void main(String[] args) {
-		ProfileAdder pfa=new ProfileAdder();
-		PostsAdder pta=new PostsAdder();
+
 		
-		pfa.createProfile();
-		pta.createPosts();
 		
 		SocialNetwork sn=new SocialNetwork();
 		sn.start("krish");
