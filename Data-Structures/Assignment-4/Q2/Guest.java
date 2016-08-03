4b665326d61b867a027eadc9e3b3851e38e83787
meta-krishan-kataria <krishan.kataria@metacube.com>
2016-08-03 18:15:38 +0530

    Add files via upload

---
+++ Data-Structures/Assignment-4/Q2/Guest.java
@@ -0,0 +1,34 @@
+package q2;
+
+
+
+// class to define guest
+public class Guest {
+	
+	// variable for guest name
+	private String guestName;
+	
+	// variable for guest age
+	private int guestAge;
+	
+	
+	public Guest(String guestName , int guestAge){
+		
+		this.guestName = guestName;
+		
+		this.guestAge = guestAge;
+	}
+	
+	// return guest name
+	public String getGuestName(){
+		
+		return this.guestName;
+	}
+	
+	// return guest age
+	public int getGuestAge(){
+		
+		return this.guestAge;
+	}
+
+}
