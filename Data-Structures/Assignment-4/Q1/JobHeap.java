9b110b40f07394ed2c83f5b818266ce6abed3e92
meta-krishan-kataria <krishan.kataria@metacube.com>
2016-08-04 11:00:19 +0530

    Update JobHeap.java

--- Data-Structures/Assignment-4/Q1/JobHeap.java
+++ Data-Structures/Assignment-4/Q1/JobHeap.java
@@ -1,8 +1,24 @@
 import java.util.List;
 
-
+/**
+ * Class to create heap for the type JOB
+ * 
+ * */
 public class JobHeap {
-	
+	/**
+	 * method to maintain the heap order
+	 * 
+	 * MAX-HEAPIFY	
+	 * (ref: Intro. to algorithm by Cormen)
+	 *	Its inputs are an array A and an index i into the array. When it is called, MAXHEAPIFY
+	 *	assumes that the binary trees rooted at LEFT(i) and RIGHT(i) are maxheaps,
+	 *		but that A[i] might be smaller than its children, thus violating the max-heap
+	 *	property. MAX-HEAPIFY lets the value at A[i] “float down” in the max-heap so
+	 *	that the subtree rooted at index i obeys the max-heap property.
+	 * 
+	 * a is the input arraylist
+	 * i is the index for the node
+	 * */
 	void maxHeapify(List<Job> a,int i){
 		//l is the index for the left child
 		int l= 2*i +1;
@@ -27,13 +43,10 @@ public class JobHeap {
 		}
 	}	
 	
-	/**
-	 * 
-	 * */	
-	
+
 	
 	/**
-	 * 
+	 * swaps the two elements x,y in the arraylist
 	 * */
 	public void swap(List<Job> a ,int x,int y){
 		Job temp=a.get(x);
@@ -44,8 +57,8 @@ public class JobHeap {
 		
 			
 	/**
-	 * build heap
-	 * 
+	 * builds heap from the input arraylist
+	 * @return created heap
 	 * */	
 	public List<Job> buildHeap(List<Job> a){
 		for( int i = a.size()/2 ; i >= 0  ; i--){
