6af6276aacb0f4c9494e6c7e110006c9b23fa6a5
meta-krishan-kataria <krishan.kataria@metacube.com>
2016-08-04 10:59:32 +0530

    Update HeapDemo.java

--- Data-Structures/Assignment-4/Q1/HeapDemo.java
+++ Data-Structures/Assignment-4/Q1/HeapDemo.java
@@ -1,15 +1,19 @@
 
+//Class to demonstrate Heap class methods
 public class HeapDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		int a[]={1,2,3,4,5,6,7};
 		
 		Heap heap=new Heap();
 	
 
-		 int b[]=heap.buildHeap(a);
-		 
+		int b[]=heap.buildHeap(a);
+		
+		for(int x : b){
+			System.out.print(x+"  ");
+		}
 		 
 	}
 
