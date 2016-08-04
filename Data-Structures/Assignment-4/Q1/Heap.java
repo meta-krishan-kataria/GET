30bd9a4fca0f0340377ed7dd24bb257cf2dfe566
meta-krishan-kataria <krishan.kataria@metacube.com>
2016-08-04 10:59:15 +0530

    Update Heap.java

--- Data-Structures/Assignment-4/Q1/Heap.java
+++ Data-Structures/Assignment-4/Q1/Heap.java
@@ -1,24 +1,20 @@
-
+/**
+ * Class to implement Heap
+ * 
+ * */
 public class Heap {
 	
-	/**
-	 * 
-		* MAX-HEAPIFY.A; i /
-		1 l D LEFT.i /
-		2 r D RIGHT.i /
-		3 if l <= A:heap-size and AOEl > AOEi
-		4 largest D l
-		5 else largest D i
-		6 if r <= A:heap-size and AOEr > AOElargest
-		7 largest D r
-		8 if largest != i
-		9 exchange AOEi with AOElargest
-		10 MAX-HEAPIFY.A; largest/
-	 * 
-	 * 
-	 * */
 
 	/**
+	 * method to maintain the heap order
+	 * 
+	 * MAX-HEAPIFY.
+		Its inputs are an array A and an index i into the array. When it is called, MAXHEAPIFY
+		assumes that the binary trees rooted at LEFT(i) and RIGHT(i) are maxheaps,
+		but that A[i] might be smaller than its children, thus violating the max-heap
+		property. MAX-HEAPIFY lets the value at A[i] “float down” in the max-heap so
+		that the subtree rooted at index i obeys the max-heap property.
+	 * 
 	 * a is the input array
 	 * i is the index for the node
 	 * */
@@ -27,7 +23,8 @@ public class Heap {
 		int l= 2*i +1;
 		//r is the index for the right child
 		int r= 2*i +2;
-		int largest;
+		
+		int largest;	//to store the index for the largest element
 		
 		if( l <= getHeapSize(a)-1 && a[l] > a[i]  ){
 			largest=l;
@@ -46,7 +43,7 @@ public class Heap {
 	}	
 	
 	/**
-	 * 
+	 * 	@return the size of the heap
 	 * */	
 	public int  getHeapSize(int[] a ){
 		int count =0;
@@ -57,7 +54,7 @@ public class Heap {
 	}
 	
 	/**
-	 * 
+	 * swaps two elements of the array
 	 * */
 	public void swap(int a[],int x,int y){
 		int temp=a[x];
@@ -67,7 +64,11 @@ public class Heap {
 		
 			
 	/**
-	 * build heap
+	 * method to build heap
+	 * 
+	 *  the elements in the subarray A[(n/2)+1....n] are all leaves of the tree, and so each is
+	 *  a 1-element heap to begin with. The procedure BUILD-MAX-HEAP goes through
+	 *	the remaining nodes of the tree and runs MAX-HEAPIFY on each one.
 	 * 
 	 * */	
 	public int[] buildHeap(int[] a){
