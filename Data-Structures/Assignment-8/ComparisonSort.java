d9fec1af6e1a63302f58d43f0c5bf1d78bd180b2
meta-krishan-kataria <krishan.kataria@metacube.com>
2016-08-10 14:36:54 +0530

    Update ComparisonSort.java

--- Data-Structures/Assignment-8/ComparisonSort.java
+++ Data-Structures/Assignment-8/ComparisonSort.java
@@ -3,8 +3,10 @@ public class ComparisonSort {
 	public static void sort(int[] input){
 		
 		if( input.length <= 10 ){
+			System.out.println("Using Bubble sort");
 			BubbleSort.sort(input);
 		}else{
+			System.out.println("Using Quick sort");
 			QuickSort.sort(input);
 		}
 		
