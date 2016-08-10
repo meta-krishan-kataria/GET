2fa40a426e6d9a31bddbfdb2803f36f800dc5248
meta-krishan-kataria <krishan.kataria@metacube.com>
2016-08-10 14:37:42 +0530

    Update BubbleSort.java

--- Data-Structures/Assignment-8/BubbleSort.java
+++ Data-Structures/Assignment-8/BubbleSort.java
@@ -2,8 +2,9 @@
 public class BubbleSort implements Sorting {
 	
 	public static void sort(int[] input){
+		
 		for(int i=0; i < input.length;  i++){
-			for(int j=0; j < input.length; j++){
+			for(int j=0; j < input.length-1; j++){
 				if( input[j] > input[j+1] ){
 					swap(input,j,j+1);
 				}
