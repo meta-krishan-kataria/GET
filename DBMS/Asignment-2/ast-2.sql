e2a906e1067f58095e4ffa545efb4d2985f0f081
meta-krishan-kataria <krishan.kataria@metacube.com>
2016-08-19 12:09:32 +0530

    Update ast-2.sql

--- DBMS/Asignment-2/ast-2.sql
+++ DBMS/Asignment-2/ast-2.sql
@@ -52,12 +52,14 @@ USE LIS;
  * accessin number, member id, member name and return date. If book is not returned,
  * keep the return date column blank. Sort the result in ascending order of issue date
  * and then ascending ordeer of member name*/
-SELECT bi.issue_dt AS 'Issue Date', bi.accession_number AS 'Accession Number',
-    bi.member_id AS 'Member ID', m.member_name AS 'Member Name', br.return_dt 
-    FROM book_issue bi LEFT JOIN 
-        book_return br ON bi.accession_number = br.accession_number
-    INNER JOIN members m ON bi.member_id = m.member_id
-    ORDER BY bi.issue_dt, m.member_name;
+SELECT bi.issue_dt , bi.accession_number, bi.member_id, m.member_name, IFNULL(br.return_dt,'Not Returned')
+FROM book_issue bi
+INNER JOIN book_return
+    ON bi.issue_dt=br.issue_dt AND 
+        bi.accession_number=br.accession_number AND
+        bi.member_id=br.member_id
+LEFT JOIN members m ON bi.member_id=m.member_id        
+ORDER BY bi.issue_dt,bi.member_name;        
 
 /* Assignment 3 */
 
