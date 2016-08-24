28e7b4e78e161b12bca50a29f2e5000cc31f62ce
meta-krishan-kataria <krishan.kataria@metacube.com>
2016-08-24 10:35:01 +0530

    Update Book.java

--- DBMS/JDBC/Book.java
+++ DBMS/JDBC/Book.java
@@ -1,1 +1,56 @@
+package jdbc;
+//POJO class 
+public class Book {
+	private int acc_no;
+	private int title_id;
+	private String name;
+	private String publisher;
+	private String author;
+	private int price;
+	
+	public int getAcc_no() {
+		return acc_no;
+	}
+	
+	public void setAcc_no(int acc_no) {
+		this.acc_no = acc_no;
+	}
+	public int getTitle_id() {
+		return title_id;
+	}
+	public void setTitle_id(int title_id) {
+		this.title_id = title_id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	
+	@Override
+	public String toString(){
+		return getName();
+	}
+	
+}
 
