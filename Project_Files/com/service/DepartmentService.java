f86976c9e7114f6fcdad3563f5314663ca605096
meta-krishan-kataria <krishan.kataria@metacube.com>
2016-10-20 11:03:06 +0530

    added more methods

--- Project_Files/com/service/DepartmentService.java
+++ Project_Files/com/service/DepartmentService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dao.DepartmentDAOImpl;
+import com.sun.jmx.snmp.Timestamp;
 import com.util.Adapter;
 import com.model.*;
 
@@ -19,39 +20,83 @@ import com.model.*;
 
 @Service
 public class DepartmentService {
-	
+
 	@Autowired
 	Adapter adapter;
-	
+
 	@Autowired
 	DepartmentDAOImpl departmentDAO;
-	
-	
+
+
 	//
 	public List<IssueVO> getNewIssues(int deptId){
-		
+
 		List<Issue> issueList = departmentDAO.getNewIssues(deptId);
-		
+
 		return adapter.convertToVO(issueList);
 	}
-	
-	
-	
+
+
+
 	//
 	public List<IssueVO> getPendingIssues(int deptId){
-		
+
 		List<Issue> issueList = departmentDAO.getPendingIssues(deptId);
-		
+
 		return adapter.convertToVO(issueList);
 	}
-	
-	
-	
+
+
+
 	public IssueVO getIssueById(int issueId){
-		
+
 		Issue issue=departmentDAO.getIssueById(issueId);
-		
+
 		return adapter.convertToVO(issue);
 	}
-	
+
+
+
+	/*public boolean approveIssue(int issueId, int deptId){
+
+		return  departmentDAO.updateStatusOfIssue(issueId, deptId, "approve");
+	}*/
+
+
+
+	public boolean markIssueForReview(int issueId, int deptId){
+
+		return  departmentDAO.updateStatusOfIssue(issueId, deptId, "conflict");
+	}
+
+
+	public List<ResolverVO> getResolversList( int deptId ){
+
+		List<UserDetails> resolvers=departmentDAO.getAllResolvers(deptId);
+
+		return adapter.convertToResolverVO( resolvers );
+	}
+
+
+
+
+	public boolean allocateIssue(  IssueVO issueVO  ){
+		//marking as "approved"
+		boolean isApproved=departmentDAO.updateStatusOfIssue(issueVO.getId(), issueVO.getDeptId(), "approve");
+
+		//assigning resolvers
+		for( Integer resolverId : issueVO.getResolvers() ){
+			departmentDAO.assignIssueToResolver( issueVO.getId(), resolverId, issueVO.getDeptId() );
+		}
+
+		//setting target resolution date
+		//departmentDAO.setTargetResolutionDateForIssue(   issueVO.getTargetResolutionDate( ), issueId)
+
+		return true;
+	}
+
+
+
+
+
 }
